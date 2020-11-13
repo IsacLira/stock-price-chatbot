db40fca0b1dd2ee6961df36c152e9f518bdea387
Isac Lira <isac_lira@apple.com>
2020-11-13 16:08:48 -0300

    Create a User class and update server.

--- server.py
+++ server.py
@@ -1,30 +1,6 @@
+from src import create_app, socketio
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-from src.message_handler import MessageHandler
-
-app = Flask(__name__)
-
-socketio = SocketIO(app)
-message_handler = MessageHandler()
-
-
-@app.route('/')
-def sessions():
-    return render_template('session.html')
-
-@socketio.on('read messages')
-def read_messages(methods=['GET', 'POST']):
-    content = message_handler.build_messages()
-    socketio.emit('response event', content)
-
-@socketio.on('user connection')
-def user_connected(json, methods=['GET', 'POST']):
-    pass
-
-@socketio.on('message event')
-def handle_messages(json, methods=['GET', 'POST']):
-    message_handler.process_payload(json)
+app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5007)
+    socketio.run(app, port=5009)
--- src/repository/user_repository.py
+++ src/repository/user_repository.py
@@ -1,4 +1,5 @@
 import json
+from flask_login import UserMixin
 from src.db.database import RedisDB
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -6,25 +7,36 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class UserRepo(RedisDB):
     def __init__(self):
         super().__init__()
+        self.user_lis = self.lrange('user', 0, -1)
 
     def set_user(self, user, password):
         password = generate_password_hash(password, method='sha256')
         user_info = {'user_name': user, 'password':password}
         self.rpush('user', user_info)
 
+    def instantiate_users(self):
+        users = {user['user_name'].encode("utf-8"): User(user['user_name'])
+                 for user in self.user_lis}
+        return users
+
+    def get_user(self, user_id):
+        res = self.user_instance[user_id]
+        return res
+
     def validate_user(self, user, password):
-        user_lis = self.lrange('user', 0, -1)
-        user_info = [info for info in user_lis if (user == info['user_name'])]
+        user_info = [info for info in self.user_lis if (user == info['user_name'])]
         if len(user_info) == 0:
             print('user not found')
             return False
-        print(user_info[0]['user_name'] == user)
         condition = (user_info[0]['user_name'] == user) & \
             check_password_hash(user_info[0]['password'], password)
         return condition
 
 
-if __name__ == '__main__':
-    user_repo = UserRepo()
-    user_repo.set_user('admin4', '123')
-    print(user_repo.validate_user('admin4', '123'))
+class User(UserMixin):
+    def __init__(self, user):
+        super().__init__()
+        self.user = user
+
+    def get_id(self):
+        return self.user.encode("utf-8")
