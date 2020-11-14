ab7f83bdb488eba99ea4c2fe6b2857972834e4aa
Isac Lira <isac_lira@apple.com>
2020-11-14 11:45:48 -0300

    Added signup functionality

---
+++ src/repository/user_signup.py
@@ -0,0 +1,22 @@
+import argparse
+from src.repository.user_repository import UserRepo
+
+
+class UserSignUp:
+    def __init__(self):
+        self.user_repo = UserRepo()
+
+    def signup(self, user, password):
+        self.user_repo.set_user(user, password)
+
+if __name__ == "__main__":
+    sgn_ = UserSignUp()
+    params = {}
+    parser = argparse.ArgumentParser(description="User signup")
+    parser.add_argument('--user', type=str)
+    parser.add_argument('--password', type=str)
+    params.update(vars(parser.parse_args()))
+
+    sgn_.signup(params['user'], params['password'])
+
+    print(sgn_.user_repo.lrange('user', 0, -1))
