aa1e5badf52661a64f0d39c3977635f8ab6f7f8e
Isac Lira <isac_lira@apple.com>
2020-11-15 07:49:00 -0300

    Avoid save user already in db

--- src/repository/user_repository.py
+++ src/repository/user_repository.py
@@ -2,6 +2,7 @@ import json
 from flask_login import UserMixin
 from src.db.database import RedisDB
 from werkzeug.security import generate_password_hash, check_password_hash
+from src.utils.logger import logger
 
 
 class UserRepo(RedisDB):
@@ -9,9 +10,18 @@ class UserRepo(RedisDB):
         super().__init__()
 
     def set_user(self, user, password):
+        users = self.get_users()
+        saved_user_bool = any([user == info['user_name']
+                               for info in users])
+        if saved_user_bool:
+            logger.info('User already in the database')
+            return False
+
         password = generate_password_hash(password, method='sha256')
         user_info = {'user_name': user, 'password': password}
         self.rpush('user', user_info)
+        logger.info('User saved successfully.')
+        return True
 
     def instantiate_users(self):
         users = {user['user_name'].encode("utf-8"): User(user['user_name'])
@@ -30,7 +40,7 @@ class UserRepo(RedisDB):
         user_lis = self.get_users()
         user_info = [info for info in user_lis if (user == info['user_name'])]
         if len(user_info) == 0:
-            print('user not found')
+            logger.info(f'User {user} not found.')
             return False
         status = (user_info[0]['user_name'] == user) & \
             check_password_hash(user_info[0]['password'], password)
